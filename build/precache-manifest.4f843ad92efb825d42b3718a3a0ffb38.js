b1a429093e91d333f787ca5c3b87aa22465f58f2
vamshidharchalla <vamshichalla@gmail.com>
2019-02-21 12:51:49 +0530

    added build

---
+++ build/precache-manifest.4f843ad92efb825d42b3718a3a0ffb38.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b265be940862d19fbc19",
+    "url": "/static/js/main.b265be94.chunk.js"
+  },
+  {
+    "revision": "e5f538796929071151cc",
+    "url": "/static/js/2.e5f53879.chunk.js"
+  },
+  {
+    "revision": "a2385cad3d9417ccbacea3a53d05118a",
+    "url": "/index.html"
+  }
+];
